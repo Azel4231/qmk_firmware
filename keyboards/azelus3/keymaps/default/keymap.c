10504cd4b5aaa1cb6bf6e070d2a97617f093ec53
Azel4231 <Azel4231@users.noreply.github.com>
2024-01-25 13:28:20 +0100

    First combo

--- keyboards/azelus3/keymaps/default/keymap.c
+++ keyboards/azelus3/keymaps/default/keymap.c
@@ -80,6 +80,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   }
 };
 
+const uint16_t PROGMEM SpitzeKlammer[] = {DE_LABK, DE_SCLN, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(SpitzeKlammer, DE_RABK)
+    // COMBO(test_combo2, LCTL(KC_Z)), // keycodes with modifiers are possible too!
+};
+
 /*
 Notizen für zukünftige Änderungen:
   * Sym Layer:
