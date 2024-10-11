2a65981ad04d9bd103aa4728a5b1e076d11060bc
Azel4231 <Azel4231@users.noreply.github.com>
2024-10-11 17:23:34 +0200

    Remove Steno, add Noted

--- keyboards/azelus3/keymaps/default/keymap.c
+++ keyboards/azelus3/keymaps/default/keymap.c
@@ -7,7 +7,7 @@
 #define _SYM 4
 #define _NAV 3
 #define _RIGHT 2
-#define _STENO 1
+#define _NOTED 1
 #define _COLEMAK 0
 
 #define _____ KC_TRNS
@@ -47,18 +47,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,     KC_Q,  KC_W,    KC_F,    KC_P,    KC_B,       KC_J,    KC_L,         KC_U,     DE_Z,   DE_UDIA, DE_ADIA},
   {KC_LCTL,    KC_A,  KC_R,    KC_S,    KC_T,    KC_G,       KC_M,    KC_N,         KC_E,     KC_I,   KC_O,    KC_BSPC},
   {MO(_RIGHT), DE_Y,  KC_X,    KC_C,    KC_D,    KC_V,       KC_K,    KC_H,         KC_COMM,  KC_DOT, DE_MINS, DE_SS},
-  {XXXXX,      XXXXX, KC_LGUI, KC_LALT, KC_LSFT, MO(_SYM),   KC_ENT,  MO(_NAV),     KC_SPC,   XXXXX /*tF(_STENO)*/,  XXXXX,   XXXXX}
+  {XXXXX,      XXXXX, KC_LGUI, KC_LALT, KC_LSFT, MO(_SYM),   KC_ENT,  MO(_NAV),     KC_SPC,   XXXXX,  XXXXX,   XXXXX}
 },
-[_STENO] =  {
-  {DF(_COLEMAK), XXXXX, XXXXX,   XXXXX,   XXXXX,   XXXXX,      XXXXX,   XXXXX,  XXXXX,   XXXXX,  XXXXX,   XXXXX},
-  {XXXXX,        KC_1,  KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,   KC_8,    KC_9,   KC_0,          KC_0},
-  {XXXXX,        KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,       DE_Z,    KC_U,   KC_I,    KC_O,   KC_P,    DE_UDIA},
-  {XXXXX,        KC_A,  KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,   KC_K,    KC_L,   DE_ODIA, DE_ADIA},
-  {XXXXX,        XXXXX, XXXXX,   XXXXX,   KC_C,    KC_V,       KC_N,    KC_M,   XXXXX,   XXXXX,  XXXXX,   XXXXX}
+[_NOTED] = {
+  {KC_ESC,     XXXXX, XXXXX,   XXXXX,   KC_F5,   XXXXX,      XXXXX,   KC_KP_SLASH,  XXXXX,    XXXXX,  XXXXX,   DE_MINS},
+  {KC_TAB,     DE_Z,  DE_Y,    KC_U,    KC_A,    KC_Q,       KC_P,    KC_B,         KC_M,     KC_L,   KC_F,    KC_J},
+  {KC_LCTL,    KC_C,  KC_S,    KC_I,    KC_E,    KC_O,       KC_D,    KC_T,         KC_N,     KC_R,   KC_H,    KC_BSPC},
+  {MO(_RIGHT), KC_V,  KC_X,    DU_UDIA, DE_ADIA, DE_ODIA,    KC_W,    KC_G,         KC_COMM,  KC_DOT, KC_K,    DE_SS},
+  {XXXXX,      XXXXX, KC_LGUI, KC_LALT, KC_LSFT, MO(_SYM),   KC_ENT,  MO(_NAV),     KC_SPC,   XXXXX,  XXXXX,   XXXXX}
 },
 [_NAV] = {
   {DF(_COLEMAK), XXXXX,   XXXXX,         XXXXX,    _____,  XXXXX,      XXXXX,   _____,   XXXXX,   XXXXX,    XXXXX, _____},
-  {DF(_STENO), DE_DEG,  KC_F7,         KC_F8,    KC_F9,  KC_F10,     KC_PGUP, KC_HOME, KC_UP,   KC_END,   _____, KC_AUDIO_VOL_UP},
+  {DF(_NOTED), DE_DEG,  KC_F7,         KC_F8,    KC_F9,  KC_F10,     KC_PGUP, KC_HOME, KC_UP,   KC_END,   _____, KC_AUDIO_VOL_UP},
   {_____,        _____,   KC_F4,         KC_F5,    KC_F6,  KC_F11,     KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, _____, KC_AUDIO_VOL_DOWN},
   {_____,        _____,   KC_F1,         KC_F2,    KC_F3,  KC_F12,     _____,   KC_INS,  XXXXX,   KC_DEL,   _____, _____},
   {XXXXX,        XXXXX,   QK_BOOTLOADER, KC_LALT,  _____,  _____,      KC_ENT,  _____,   _____,   _____,    XXXXX, XXXXX}
